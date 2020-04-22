c1238593ceb10b237419bb75c6099148daa3a2ba
otgooollo <61413991+otgooollo@users.noreply.github.com>
2020-04-23 00:49:17 +0800

    Add files via upload

---
+++ ngram urdun.py
@@ -0,0 +1,40 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:\Users\ollo\Downloads\ngram.py =================
+['it']
+['it', 'was']
+['it', 'was', 'the', 'best']
+['it', 'was', 'the', 'best', 'of', 'times']
+---------------------------------------------------------------
+['it', 'was', 'the', 'best', 'of']
+['was', 'the', 'best', 'of', 'times']
+['the', 'best', 'of', 'times', 'it']
+['best', 'of', 'times', 'it', 'was']
+['of', 'times', 'it', 'was', 'the']
+['times', 'it', 'was', 'the', 'worst']
+['it', 'was', 'the', 'worst', 'of']
+['was', 'the', 'worst', 'of', 'times']
+['the', 'worst', 'of', 'times', 'it']
+['worst', 'of', 'times', 'it', 'was']
+['of', 'times', 'it', 'was', 'the']
+['times', 'it', 'was', 'the', 'age']
+['it', 'was', 'the', 'age', 'of']
+['was', 'the', 'age', 'of', 'wisdom']
+['the', 'age', 'of', 'wisdom', 'it']
+['age', 'of', 'wisdom', 'it', 'was']
+['of', 'wisdom', 'it', 'was', 'the']
+['wisdom', 'it', 'was', 'the', 'age']
+['it', 'was', 'the', 'age', 'of']
+['was', 'the', 'age', 'of', 'foolishness']
+['the', 'age', 'of', 'foolishness']
+['age', 'of', 'foolishness']
+['of', 'foolishness']
+['foolishness']
+---------------------------------------------------------------
+[['it', 'was', 'the', 'best', 'of'], ['was', 'the', 'best', 'of', 'times'], ['the', 'best', 'of', 'times', 'it'], ['best', 'of', 'times', 'it', 'was'], ['of', 'times', 'it', 'was', 'the'], ['times', 'it', 'was', 'the', 'worst'], ['it', 'was', 'the', 'worst', 'of'], ['was', 'the', 'worst', 'of', 'times'], ['the', 'worst', 'of', 'times', 'it'], ['worst', 'of', 'times', 'it', 'was'], ['of', 'times', 'it', 'was', 'the'], ['times', 'it', 'was', 'the', 'age'], ['it', 'was', 'the', 'age', 'of'], ['was', 'the', 'age', 'of', 'wisdom'], ['the', 'age', 'of', 'wisdom', 'it'], ['age', 'of', 'wisdom', 'it', 'was'], ['of', 'wisdom', 'it', 'was', 'the'], ['wisdom', 'it', 'was', 'the', 'age'], ['it', 'was', 'the', 'age', 'of'], ['was', 'the', 'age', 'of', 'foolishness']]
+---------------------------------------------------------------
+[['here', 'are', 'four'], ['are', 'four', 'words']]
+---------------------------------------------------------------
+[['this', 'test', 'sentence', 'has', 'eight'], ['test', 'sentence', 'has', 'eight', 'words'], ['sentence', 'has', 'eight', 'words', 'in'], ['has', 'eight', 'words', 'in', 'it']]
+>>> 
